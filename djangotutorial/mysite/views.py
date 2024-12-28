f34c6941965c5620cb2cb315aae07962b743ce21
Ваше Имя <shterbax@gmail.com>
2024-12-29 00:38:27 +0200

    Create model Article and add Article in API View

--- djangotutorial/mysite/views.py
+++
@@ -1,9 +0,0 @@
-from django.http import HttpResponse
-from rest_framework import status
-from rest_framework.decorators import api_view
-from rest_framework.response import Response
-
-
-@api_view(["GET"])
-def hello(request):
-    return Response(data={"Message": "Hello world"}, status=status.HTTP_200_OK)
